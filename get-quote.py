39ba16a444f5db67d1d304e92e4a3df89b5295ba
LAPTOP-UGRJGV82\Haris <Haris.Tiew@gmail.com>
2021-08-14 13:14:48 +0800

    Update get-quote.py

--- get-quote.py
+++ get-quote.py
@@ -1,11 +1,11 @@
 def running():
   print("Keep it logically awesome.")
 
-  #f = open("quotes.txt")
-  #quotes = f.readlines()
-  #f.close()
+  f = open("quotes.txt")
+  quotes = f.readlines()
+  f.close()
 
-  #print(quotes)
+  print(quotes[0])
 
 if __name__== "__running__":
   running()
